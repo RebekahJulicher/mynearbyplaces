d32953eac43d4c519305fc9110a4600ba4a67114
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-12-15 10:17:16 -0700

    last build

--- src/ServerInterface/server.js
+++ src/ServerInterface/server.js
@@ -9,38 +9,17 @@ let addPlace = (name, category, city, state) => {
     .then(x => x.json()));
 }
 
-// Unchanged because I haven't figured out how to do this yet
 let search = (city, state, category) => {
-    let result = [];
-    if (city.length > 0) {
-        result = places.filter(x => x.city === city);
-    }
-    if (state.length > 0) {
-        if (city.length > 0){
-            result = result.filter(x => x.state === state);
-        }
-        else {
-            result = places.filter(x => x.state === state);
-        }
-    }
-    if (category.length > 0) {
-        if (city.length > 0 || state.length > 0){
-            result = result.filter(x => x.category === category);
-        }
-        else {
-            result = places.filter(x => x.category === category);
-        }
-    }
-    if (city.length === 0 && state.length === 0 && category.length === 0){
-        return places;
-    }
-    return result;
+    let data = {city: city, state: state, category: category};
+    return(fetch (API + "/review", {method: "GET", headers: {"CONTENT-TYPE": "application/json"}, body: JSON.stringify(data)})
+    .then(x => x.json()));
 }
 
 let addReview = (place, review) => {
-    //This doesn't use ids because I haven't gotten that sorted yet.
+    //This I don't think I actually pass this an id yet, so this won't work 
+    let data = {place: place.placeid, review: review};
     return(fetch(API + '/review/', 
-    {method: "POST", headers: {"CONTENT-TYPE": "application/json"}, body: JSON.stringify(review)})
+    {method: "POST", headers: {"CONTENT-TYPE": "application/json"}, body: JSON.stringify(data)})
     .then(x => x.json()));
 }
 
